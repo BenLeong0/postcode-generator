f7448fe7388a39506d1c6b778e987ef2e3ae444f
benleong0 <benleong@myurbanjungle.com>
2022-04-21 19:26:28 +0100

    css

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 var md5 = require('md5');
 
 function App() {
+  const [prefix, setPrefix] = useState<string>("");
+  const [state, setState] = useState<string>("getVariant");
 
+  const isVariant = (postcode: string): boolean => {
+    return parseInt(md5(postcode), 16) % 1000 > 500;
+  }
+
+  const getValidSuffix = (): string => {
+    for (let char of 'ABCDEFGHIJKLMNOPQRSTUVWXYZ') {
+      var suffix = "1A" + char;
+      var fullPostcode = prefix + suffix;
+      if (isVariant(fullPostcode.replace(" ", "").toUpperCase())) {
+        return suffix;
+      }
+    }
+    return "";
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <p className="margin-bottom-large">
           Input a prefix, and get a variant postcode.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React {md5("Hiii")}
-        </a>
+        <input
+          className="prefix-input margin-bottom-large"
+          value={prefix}
+          onChange={(e: any) => setPrefix(e.target.value)}
+        />
+        <div>
+          <p className="italic">Sample variant postcode:</p>
+          <p className="sample-postcode">{prefix.toUpperCase()} {getValidSuffix()}</p>
+        </div>
       </header>
     </div>
   );
